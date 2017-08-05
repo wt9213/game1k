c1522fef57b79e2c5b219496da67ce3016af542c
fc01 <fc01@fc01deMacBook-Pro.local>
2017-08-05 17:24:48 +0800

    ``

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # game1k
+
+
+## [Play It Here](https://fc01.github.io/game1k)
+
+## html
+```html
+ <canvas id="c" width="300" height="500"></canvas>
+```
+
+
+## js
+```js
+_="c=c.getCt'2d'Wc.ft='px Tahoma';u=tUs=wQrP54gOhsl('+v*35+',%,'+n+'%)'Iqzpz9y:zyIzon,yIzn=t;Ikj!#={y:~~(n/6),x:n%6I}Ikeydownv=zkeyCode;Z(w=vK6501872683383?3:2)IhOrgba(LLL0.2)';GLL30L0Wt;eP6uQdQr.forEach(Ax=n%6;jNZw<0XV$YJV$YJA=_-1_-31:0;j_-2XHY+3$YJv)V$VJ}Y$v=V+AJHs=x;}vZB98WDZN+sZn<51+s)q(Y${9y:1Li:Math.random()>0.52,m:Ll:0},98WjvXj_3XY]Uj!#Xr[V]*6+x$v;B9V])}D5Z#Z)B.9.yWj6Z#Z!X#U	D4Xfor(iUi<(xK0||xK54WiXA=[6,1,7,-5Ja=Y+b=Y-jaZbZaZbUk(a,nWk(b,nWu=15;}NZu!=15Xu=27}!(NZ7Zt%>20)Xf=(t-zn)/10;jf>1)fQzx=@xx-@xzy=@yy-@yx=*zx;y=4-*zy;i=	;65WG9y,5LW20Wc^T	,x2,y+)}}}Ww=w<0?2:||7?w:_14?u:6?3:w;requestAnimatiFrame(h)Ih()=(v,n,y,A)=>{zv.jif(_wK^.fillZ&&Yr[nX){W);Ve[xU=0;Q=1;P=new Array(Oc^Style='Nn>47L0,K==J];I};HjdZvXdUGc^Rect(D_1Bq(v,@zp.9x,$]=#zm+1_2zl+(zo..iK	A[i]J)^(0W50?1::vK}jg(i,={x:)*f;Zs!=X#Qext(on?-40	zi++";for(Y in $="	#$9@BDGHIJKLNOPQUVWXYZ^_jz")with(_.split($[Y]))_=join(pop());eval(_)
+```
---
+++ build.js
@@ -0,0 +1,135 @@
+
+let cccc = 0;
+
+const getMaxSave = (sourceCode) => {
+    let arr = sourceCode.split('');//!!!!!!!!!!
+    let saveArr = [];// as { str: string, count: number, save: number }[];
+
+    let f = (n) => {
+        let dic = Object.create(null);
+
+        arr.forEach((v, i) => {
+            if (i < arr.length - n + 1) {
+                v = arr.slice(i, i + n).join('');
+                if (dic[v] == null) dic[v] = 0;
+                dic[v]++;
+            }
+        });
+
+        // a | b | c | AAAA
+
+        for (let k in dic) {
+            let count = dic[k];
+            if (count != 1) {
+                saveArr.push({
+                    str: k,
+                    count,
+                    save: n * count - count - n - 1//
+                })
+            }
+        }
+    }
+
+    for (let i = 2; i < 32; i++) {//
+        f(i);
+    }
+
+    saveArr.sort((a, b) => b.save - a.save);
+    if (saveArr[0] && saveArr[0].save > 0)
+        return saveArr[0];
+    else
+        return null;
+}
+
+
+const getTable = (sourceCode) => {
+    let table = [];//string[]ƒ
+    for (let i = 1; i < 128; i++) {
+        let s = String.fromCharCode(i);
+        if (
+            i != 96 &&
+            i != 10 &&
+            i != 13 &&
+            i != 34 &&
+            i != 39 &&
+            i != 92 && sourceCode.indexOf(s) == -1) // ` \
+            table.push(s);
+    }
+    return table;
+}
+
+function ys(sourceCode) {
+    let table = getTable(sourceCode);
+    let tableStr = '';
+    while (table.length > 0) {
+        let oneChar = table.pop();
+        let obj = getMaxSave(sourceCode);
+        if (obj == null) break;
+        let s = obj.str;
+        console.log(`${++cccc} ${s}      ${oneChar}`);
+        tableStr += oneChar;
+        sourceCode = sourceCode.split(s).join(oneChar) + oneChar + s;
+    }
+    tableStr = tableStr.split('').reverse().join('');
+    let output = '_="' + sourceCode + '";for(Y in $="' + tableStr + '")with(_.split($[Y]))_=join(pop());eval(_)';
+    return output;
+}
+
+
+
+let fs = require('fs');
+
+
+let s = fs.readFileSync('src.js', 'utf-8');
+
+//去单行注释
+s = s.replace(/\/\/.*/g, '');
+
+//去空格回车
+s = s.replace(/new /g, 'yyyyyyyyyyyyyyy');
+s = s.replace(/40px /g, 'zzzzzzzzzzzzzz');
+
+s = s.replace(/[ \r\n]+/g, '');
+s = s.replace(/yyyyyyyyyyyyyyy/g, 'new ');
+s = s.replace(/zzzzzzzzzzzzzz/g, '40px ');
+
+
+let ok = 'abdefghklmnopqrstuw'.split('');
+
+let r = (a, b) => s = s.split(a).join(b);
+
+
+r('状态_左_', -1);
+r('状态_上_', -2);
+r('状态_右_', -3);
+
+r('状态_开始_', 1);
+r('状态_正常_', 2);
+
+r('状态_开始下落_', 3);
+r('状态_下落中_', 4);
+
+r('状态_结束下落_', 14);
+
+r('状态_爆炸_', 15);
+r('状态_爆炸中_', 16);
+
+r('状态_爆炸结束_', 26);
+r('状态_游戏结束_', 27);
+
+
+//关键词
+[
+    '状态', '结束下落标记', '现在时间', '出现方块x坐标',
+    '地图',
+    '动画到', '动画开始位置', '动画结束位置', '动画开始时间',
+    '是结束', '到xy', '结束',
+    '渲染', '填充颜色设置', '百分比', '数组', '标记'
+].forEach(v => {
+    s = s.split(v).join(ok.pop());
+});
+
+
+s = ys(s);
+fs.writeFileSync('index.js', s);
+console.log(s.length);
