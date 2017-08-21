e7e3ad3ee821d5632ed043b60e01abc80ab0b6cb
Gillpft <2285167532@qq.com>
2017-08-21 17:45:47 +0800

    ``

--- README.md
+++ README.md
@@ -2,15 +2,10 @@
 
 1KB JavaScript 代码编写的游戏
 
-按键 W S A D 移动
+按键 W S A D 操作
 
 3个或以上连续的会爆炸 
 
 玩到数字 `8` 过关
 
-#### 1KB 版本 (不支持手机)
 [Play It Here](https://fc01.github.io/game1k)
-
-
-#### 玩不过的试下这个版本  按住空格 时光倒流
-[Play It Here](https://fc01.github.io/game1k/2k.html)
--- src/build.js
+++ src/build.js
@@ -47,12 +47,10 @@ const getTable = (sourceCode) => {
     for (let i = 1; i < 128; i++) {
         let s = String.fromCharCode(i)
         if (
-            i != 96 &&
-            i != 10 &&
-            i != 13 &&
-            i != 34 &&
-            i != 39 &&
-            i != 92 && sourceCode.indexOf(s) == -1) // ` \
+            i != '`'.charCodeAt(0) &&
+            i != '\\'.charCodeAt(0) &&
+            i != '\r'.charCodeAt(0) &&
+            sourceCode.indexOf(s) == -1)
             table.push(s)
     }
     return table
@@ -66,12 +64,12 @@ const compress = sourceCode => {
         let obj = getMaxSave(sourceCode)
         if (obj == null) break
         let s = obj.str
-        console.log(`${++cccc} ${s}      ${oneChar}`)
+        console.log(`${++cccc} "${s}"  ${oneChar.charCodeAt(0)} count:${obj.count}  save:${obj.save}`)
         tableStr += oneChar
         sourceCode = sourceCode.split(s).join(oneChar) + oneChar + s
     }
     tableStr = tableStr.split('').reverse().join('')
-    let output = '_="' + sourceCode + '";for(Y in $="' + tableStr + '")with(_.split($[Y]))_=join(pop());eval(_)'
+    let output = '_=`' + sourceCode + '`;for(Y in $=`' + tableStr + '`)with(_.split($[Y]))_=join(pop());eval(_)'
     return output
 }
 
@@ -82,54 +80,64 @@ let fs = require('fs')
 
 let s = fs.readFileSync('src.js', 'utf-8')
 
+
+//
+s = s.replace(/\/\/start[\s\S\r\n]+?\/\/end/g, '')
+
 //去单行注释
 s = s.replace(/\/\/.*/g, '')
 
-//去空格回车
-s = s.replace(/new /g, 'yyyyyyyyyyyyyyy')
+// //去空格回车
 s = s.replace(/40px /g, 'zzzzzzzzzzzzzz')
-
 s = s.replace(/[ \r\n]+/g, '')
-s = s.replace(/yyyyyyyyyyyyyyy/g, 'new ')
 s = s.replace(/zzzzzzzzzzzzzz/g, '40px ')
 
 
-let ok = 'abdefghklmnopqrstuw'.split('')
+
+
+
+let unesed = 'defghklmopqrstuw'
+let ok = unesed.split('')
 
 let r = (a, b) => s = s.split(a).join(b)
 
-
 r('状态_左_', -1)
-r('状态_上_', -2)
-r('状态_右_', -3)
+r('状态_右_', 0)
 
 r('状态_开始_', 1)
 r('状态_正常_', 2)
 
 r('状态_开始下落_', 3)
-r('状态_下落中_', 4)
-
-r('状态_结束下落_', 14)
+r('状态_结束下落_', 14)//--> top 爆炸 end
 
 r('状态_爆炸_', 15)
-r('状态_爆炸中_', 16)
+r('状态_爆炸结束_', 26)//--> 开始下落
 
-r('状态_爆炸结束_', 26)
 r('状态_游戏结束_', 27)
 
 
-//关键词
-;[
-    '状态', '结束下落标记', '现在时间', '出现方块x坐标',
-    '地图',
-    '动画到', '动画开始位置', '动画结束位置', '动画开始时间',
-    '是结束', '到xy', '结束',
-    '渲染', '填充颜色设置', '百分比', '数组', '标记'
-].forEach(v => {
-    s = s.split(v).join(ok.pop())
-})
+let abc = unesed.split('')
+
+    ;[
+        '动画开始位置x', '动画开始位置y',
+        '动画结束位置x', '动画结束位置y',
+        '动画开始时间', '是结束',
+        '到xy', '到x', '到y'
+    ].forEach(v => {
+        s = s.split(v).join(abc.pop())
+    })
+
+    //关键词
+    ;[
+        '结束下落标记',
+        '状态', '现在时间', '出现方块x坐标',
+        '地图', '动画到',
+        '渲染', '时光', '倒流', '计数器'
+    ].forEach(v => {
+        s = s.split(v).join(ok.pop())
+    })
 
 
 s = compress(s)
-fs.writeFileSync('./../2k.js', s)
-console.log(s.length)
+fs.writeFileSync('./../index.js', s)
+console.log(s.length, s.length - 1024)
